500162f71e3474891dbd81ee5c949196e16ab5f7
Paul Yélamo G <pyelamo@wepa-hub.com>
2020-04-27 16:14:48 -0400

    Updating Functions
    Last Version 1.0

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,39 @@
+// Loading validations and info from local storage when DOM is ready
+document.addEventListener('DOMContentLoaded', loading());
 
-// Adding new Activity
 
+// Loading activities from local storage
+function loading(){
+     let activities;
+
+     document.querySelector('#add').disabled = true;
+     activities = readLocalStorage();
+     if (activities != null)
+     {
+          activities.forEach(element => {
+               addActivity(element);
+          });
+     }
+};
+
+// Validation quantity of letters on new activity
+document.querySelector('#activity').addEventListener('keydown', function(e){
+     if (document.querySelector('#activity').value.length > 2)
+     {
+          document.querySelector('#add').disabled = false;
+     }
+});
+
+// Adding Event Listener over Form new Activity
 document.querySelector('#add').addEventListener('click', function(e){
      e.preventDefault();
      const newActivity = document.querySelector('#activity').value;
-     saveActivity(newActivity);
      addActivity(newActivity);
+     saveActivity(newActivity);
+     document.querySelector('#activity').value = "";
 });
 
 // Creating New Activity on Pending List.
-
 function addActivity(message){
 
      const ubication = document.getElementById('pending');
@@ -21,47 +45,46 @@ function addActivity(message){
 // Adding new activity to Local Storage
 function saveActivity(message){
      let activities;
+     let content = readLocalStorage(); 
 
-     activities = readLocalStorage();
-
-     if (activities.length > 0){
-          activities.push(message);
-     }
-     else{
-          localStorage.setItem('activities', message);
-     }
-
-     
+     content.push(message);
+     localStorage.setItem('activities', JSON.stringify(content));
 }
 
 
 // Delete from local Storage
-
 document.querySelector('#pending').addEventListener("click", function(e){
      e.preventDefault;
      
      if (e.target.title == "delete" || e.target.name == "delete")
      {
-          console.log("ELIMINAR DEL LOCAL STORAGE");
+          let memory = readLocalStorage();
+          let toDelete = e.target.parentElement.parentElement.innerText;
+          toDelete = toDelete.substring(0, toDelete.length -2 );
+          
+          memory.forEach(function(element, index){
+               console.log("ELEMENT: " + element + " " + typeof element);
+               console.log("A ELIMINAR: "+toDelete + " " + typeof toDelete);
+               if( element == toDelete)
+               {
+                    console.log("ENTRE");
+                    memory.splice(index, 1);
+                    console.log("SPLICE:"+memory);
+               }
+          });
 
+          localStorage.setItem('activities', JSON.stringify(memory));
      }
-
 });
 
-function delelteActivity (){
-     let activity;
-
-}
-
 // Reading from Local Storage
-
 function readLocalStorage() {
      let activities;
 
      activities = localStorage.getItem('activities');
 
      if (activities != null){
-          return activities;
+          return JSON.parse(activities);
      }
      else{
           return activities = [];
