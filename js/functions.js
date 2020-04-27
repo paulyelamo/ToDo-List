cacfa0ac8b0dc6c27753a53fdd4c51c27118c17c
Paul Yélamo G <pyelamo@wepa-hub.com>
2020-04-27 16:08:18 -0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,15 @@
-// Loading validations and info from local storage when DOM is ready
-document.addEventListener('DOMContentLoaded', loading());
 
+// Adding new Activity
 
-// Loading activities from local storage
-function loading(){
-     let activities;
-
-     document.querySelector('#add').disabled = true;
-     activities = readLocalStorage();
-     if (activities != null)
-     {
-          activities.forEach(element => {
-               addActivity(element);
-          });
-     }
-};
-
-// Validation quantity of letters on new activity
-document.querySelector('#activity').addEventListener('keydown', function(e){
-     if (document.querySelector('#activity').value.length > 2)
-     {
-          document.querySelector('#add').disabled = false;
-     }
-});
-
-// Adding Event Listener over Form new Activity
 document.querySelector('#add').addEventListener('click', function(e){
      e.preventDefault();
      const newActivity = document.querySelector('#activity').value;
-     addActivity(newActivity);
      saveActivity(newActivity);
-     document.querySelector('#activity').value = "";
+     addActivity(newActivity);
 });
 
 // Creating New Activity on Pending List.
+
 function addActivity(message){
 
      const ubication = document.getElementById('pending');
@@ -45,46 +21,47 @@ function addActivity(message){
 // Adding new activity to Local Storage
 function saveActivity(message){
      let activities;
-     let content = readLocalStorage(); 
 
-     content.push(message);
-     localStorage.setItem('activities', JSON.stringify(content));
+     activities = readLocalStorage();
+
+     if (activities.length > 0){
+          activities.push(message);
+     }
+     else{
+          localStorage.setItem('activities', message);
+     }
+
+     
 }
 
 
 // Delete from local Storage
+
 document.querySelector('#pending').addEventListener("click", function(e){
      e.preventDefault;
      
      if (e.target.title == "delete" || e.target.name == "delete")
      {
-          let memory = readLocalStorage();
-          let toDelete = e.target.parentElement.parentElement.innerText;
-          toDelete = toDelete.substring(0, toDelete.length -2 );
-          
-          memory.forEach(function(element, index){
-               console.log("ELEMENT: " + element + " " + typeof element);
-               console.log("A ELIMINAR: "+toDelete + " " + typeof toDelete);
-               if( element == toDelete)
-               {
-                    console.log("ENTRE");
-                    memory.splice(index, 1);
-                    console.log("SPLICE:"+memory);
-               }
-          });
+          console.log("ELIMINAR DEL LOCAL STORAGE");
 
-          localStorage.setItem('activities', JSON.stringify(memory));
      }
+
 });
 
+function delelteActivity (){
+     let activity;
+
+}
+
 // Reading from Local Storage
+
 function readLocalStorage() {
      let activities;
 
      activities = localStorage.getItem('activities');
 
      if (activities != null){
-          return JSON.parse(activities);
+          return activities;
      }
      else{
           return activities = [];
